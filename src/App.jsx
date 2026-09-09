90cd3d8d3d2ede2f4be0483b4a0eb44941518f10
rocky ishimwe <rockyishimwe9@gmail.com>
2026-09-09 22:03:36 +0200

    going for hero section implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awwards</h1>
+      <h1 className="text-5xl text-violet-500 font-bold">Welcome to Awwards</h1>
     </main>
   )
 }

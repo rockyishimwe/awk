d5398264b4b3c2563aaef0ecb8e4e0e159d3a434
rocky ishimwe <rockyishimwe9@gmail.com>
2026-09-11 11:38:05 +0200

    starting imeplementing animated title section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,10 @@
+import {gsap} from "gsap";
+import {useEffect,useRef} from "react";
+import {scrollTrigger} from "gsap/ScrollTrigger";
+import clsx from "clsx";
+gsap.registerPlugin(scrollTrigger);
+
+const AnimatedTitle = ({title,containerClass})=>{
+
+}
+export default AnimatedTitle;

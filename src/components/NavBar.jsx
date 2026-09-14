909010f111d7202cfd2929da876956a8948c1c60
rocky ishimwe <rockyishimwe9@gmail.com>
2026-09-14 13:53:14 +0200

    started navbar section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,23 @@
+import clsx from "clsx";
+import gsap from "gsap";
+import {useWindowScroll} from "react-use";
+import {useEffect, useRef, useState} from "react";
+import {TiLocationArrow} from "react-icons/ti";
+import Button from "./Button";
+
+const navItems = ["Nexus","Vault","Prologue","About","Contact"];
+
 const Navbar = () => {
-  return (
-    <header className="fixed left-0 top-0 z-50 w-full">
-      <nav className="flex size-full items-center justify-between p-4">
-        <a href="#home" className="text-xl font-bold text-black">
-          ZENTRY
-        </a>
-        {/* TODO: implement nav links */}
-      </nav>
-    </header>
-  );
-};
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [isIndicatorActive, setIsIndicatorActive] =  useState(false); 
+
+  const audioElementRef = useRef(null);
+  const navContainerRef = useRef(null);
+
+
+  const {y:currentScrollY} = useWindowScroll();
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [lastScrollY,setLastScrollY] = useState(0);
+}
 
 export default Navbar;
